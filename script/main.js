ab35adc4e2fd5f8cb5fbb7af1f663f973e6d666f
PvrkFvmily <wlsgnl9973@gmail.com>
2022-12-01 10:58:53 -0800

    added initial planning comments

--- script/main.js
+++ script/main.js
@@ -1,1 +1,35 @@
 console.log('linked')
+
+// TITLE SCREEN
+// Render game screen with title, instructions, and play button
+// Dom for instructions and play button with eventlistener click to invoke function
+
+// GAME START
+// Create canvas 2d
+
+
+// Create Class for the map
+
+
+// Render stage with Super Scrub Block
+// Create Character Class
+
+
+
+
+// Move with WASD keys 
+
+
+
+
+// Stop when SSB hits a wall
+
+
+
+
+// When SBB goes over the grid changes color 
+
+
+
+
+// Win screen or next stage scree when pool is clean

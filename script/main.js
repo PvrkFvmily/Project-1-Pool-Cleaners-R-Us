a376cecf1b3c0e01d0f57fa1d3c6f7096541e1c3
PvrkFvmily <wlsgnl9973@gmail.com>
2022-12-01 12:29:32 -0800

    added sections and details to plan comments

--- script/main.js
+++ script/main.js
@@ -1,35 +1,52 @@
 console.log('linked')
 
-// TITLE SCREEN
+// --- TITLE SCREEN ---
+
 // Render game screen with title, instructions, and play button
 // Dom for instructions and play button with eventlistener click to invoke function
 
-// GAME START
+// --- GAME START ---
+
 // Create canvas 2d
 
-
-// Create Class for the map
-
+// Create Class for the map?
+// pick x coordinate that is divisble by 50px(box size)
+// render size 50px X 50px
+// function to render next stage when win condition is met
 
 // Render stage with Super Scrub Block
+
 // Create Character Class
+// Generate hit box (same size as SSB)
+// Generate color hit box (smaller size than SSB)
+// Render Character
+
+// --- GAME LOOP ---
+
+// set interval
+// set hit -- wall detection -- callback function
+// set color blue the grids where SSB has collided with
+// colored floor dection ignore when it is already clean
 
 
-
+//  --- MOVEMENT ---
 
 // Move with WASD keys 
+// function with movement 
+// ONLY MOVE ONE DIRECTION AT A TIME (disable movement when one movement is keyed)
+// can move when you hit a wall (hit dectection)
+
+// Stop when SSB hits a wall --
+// collision detect function
+
+// --- CALLBACK FUNCTIONS ---
+// -- wall detection --
+// block player from going the direction that is blocked
+// disable key stroke on the side that is being collided
 
 
+// Win screen or next stage screen when pool is clean
 
-
-// Stop when SSB hits a wall
-
-
-
-
-// When SBB goes over the grid changes color 
-
-
-
-
-// Win screen or next stage scree when pool is clean
+// --- QUESTIONS ---
+// do i need child class?
+// for game loop interval time what is a good refresh interval time
